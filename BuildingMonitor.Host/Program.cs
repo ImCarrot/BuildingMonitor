cf61529d114e772e2dc39e415280380de5604eb2
Aditya Sharma <aditya.sharma@mapmyindia.co.in>
2018-12-12 00:11:48 +0530

    Finalized code and Host Application to run on CMD

---
+++ BuildingMonitor.Host/Program.cs
@@ -0,0 +1,63 @@
+﻿using Akka.Actor;
+using BuildingMonitor.Actors;
+using BuildingMonitor.Messages;
+using System;
+using System.Threading.Tasks;
+
+namespace BuildingMonitor.Host
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (var system = ActorSystem.Create("buildingMonitoringSystem"))
+            {
+                IActorRef floorManager = system.ActorOf(FloorsManager.Prop(), "floorManager");
+
+                await CreateSimulatedSensors(floorManager);
+
+                Console.WriteLine("Systems up and running!");
+                while (true)
+                {
+                    Console.WriteLine("Press Enter to fetch data & Q to Quit");
+                    var command = Console.ReadKey();
+
+                    if (command.Key == ConsoleKey.Q)
+                        Environment.Exit(0);
+
+                    await DisplayTemperatures(system);
+                }
+            }
+        }
+
+        private static async Task DisplayTemperatures(ActorSystem system)
+        {
+            var temps = await system.ActorSelection("akka://buildingMonitoringSystem/user/floorManager/floor-1st")
+                .Ask<RespondAllTemeratures>(new RequestAllTemperatures(0));
+
+
+
+            foreach (var temp in temps.TemperatureReadings)
+            {
+                if (temp.Value is TemperatureAvailable value)
+                    Console.WriteLine($"Sensor {temp.Key} {temp.Value.GetType().Name} {value.Temperature: 00.0}");
+                else
+                    Console.WriteLine($"Sensor {temp.Key} {temp.Value.GetType().Name}");
+            }
+        }
+
+        private static async Task CreateSimulatedSensors(IActorRef floorManager)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                var newSimulator = new SimulatedSensor("1st", i.ToString(), floorManager);
+                await newSimulator.Connect();
+
+                if (i != 3)
+                {
+                    newSimulator.StartSendingSimulatedReadings();
+                }
+            }
+        }
+    }
+}
---
+++ BuildingMonitor.Host/SimulatedSensor.cs
@@ -0,0 +1,47 @@
+﻿using Akka.Actor;
+using BuildingMonitor.Messages;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BuildingMonitor.Host
+{
+    public class SimulatedSensor
+    {
+        private IActorRef floorManager;
+        private readonly Random temperatureGenerator;
+        private readonly string floorId;
+        private readonly string sensorId;
+        private IActorRef sensorRef;
+        private Timer timer;
+
+        public SimulatedSensor(string floorId, string sensorId, IActorRef floorManager)
+        {
+            this.floorManager = floorManager;
+            this.floorId = floorId;
+            this.sensorId = sensorId;
+            temperatureGenerator = new Random(int.Parse(sensorId));
+        }
+
+        public async Task Connect()
+        {
+            var response = await floorManager.Ask<RespondSensorRegistered>(
+                new RequestRegisterTemperatureSensor(1, floorId, sensorId));
+
+            sensorRef = response.SensorReference;
+
+        }
+
+        public void StartSendingSimulatedReadings()
+        {
+            timer = new Timer(SimulateUpdateTemperature, null, 0, 1000);
+        }
+
+        private void SimulateUpdateTemperature(object state)
+        {
+            var randomTemerature = temperatureGenerator.NextDouble();
+            randomTemerature *= 100;
+            sensorRef.Ask(new RequestUpdateTemperature(0, randomTemerature));
+        }
+    }
+}
